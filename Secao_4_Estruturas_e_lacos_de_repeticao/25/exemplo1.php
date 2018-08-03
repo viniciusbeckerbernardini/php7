5c1c8fe57918b581445981eaa77537523275a210
viniciusbeckerbernardini <vbkmma@gmail.com>
2018-08-03 14:05:14 -0300

    Este é um exemplo de vetor no php

---
+++ Secao_4_Estruturas_e_lacos_de_repeticao/25/exemplo1.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Neste exemplo veremos arrays e vetores, o que são arrays? são listas encadeadas que contem diversas informações.
+*/
+// Neste exemplo não temos um array e sim um vetor pois para considerar array é necessário mais de uma dimensão
+$frutas = [
+  'Morango',
+  'Laranja',
+  'Abacaxi',
+  'Melancia'
+];
+
+print_r($frutas);
+ ?>

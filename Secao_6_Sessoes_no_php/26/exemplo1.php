01db95fb03256c2ffeec1ab783d18171ef03542b
viniciusbeckerbernardini <vbkmma@gmail.com>
2018-08-06 13:23:03 -0300

    Trabalhando com sessões e com funções para sessões

---
+++ Secao_6_Sessoes_no_php/26/exemplo1.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Neste exemplo vamos falar sobre funções para sessões,
+veja as funções completas neste link:
+http://php.net/manual/pt_BR/ref.session.php
+*/
+session_start();
+echo session_save_path();
+echo session_status();
+switch(session_status()){
+  case 0:
+  echo "PHP_SESSION_DISABLED";
+  break;
+  case 1:
+  echo "PHP_SESSION_NONE";
+  break;
+  case 2:
+  echo "PHP_SESSION_ACTIVE";
+}
+ ?>

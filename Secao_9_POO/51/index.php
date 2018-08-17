463ac3a25338a7c782c07d8822ab8e96be2fb14d
viniciusbeckerbernardini <vbkmma@gmail.com>
2018-08-16 23:56:55 -0300

    Entendo namespace, refiz a explicação, acho que entendi

--- Secao_9_POO/51/index.php
+++ Secao_9_POO/51/index.php
@@ -1,6 +1,8 @@
 <?php 
 require_once('config.php');
+ 
 
+// Para usar o NameSpace usamos a palavra reservada "use" seguido do nome do namespace/nomedaclasse
 use Cliente\Cadastro;
 
 $cad = new Cadastro();
@@ -8,6 +10,6 @@ $cad = new Cadastro();
 $cad->setNome("Vini");
 $cad->setEmail('vini@vini.com');
 $cad->setSenha('132456');
-
+$cad->setEpica("sim");		
 $cad->registrarVenda();
  ?>
--- Secao_9_POO/51/model/Cliente/Cadastro.class.php
+++ Secao_9_POO/51/model/Cliente/Cadastro.class.php
@@ -1,9 +1,31 @@
 <?php 
-
+/* 
+Neste exemplo iremos falar de namespaces, mas, o que são namespaces? 
+Namespaces nada mais são do que espaços reservados onde podemos criar e organizar 
+classes em subpastas mesmo que elas tenham o mesmo nome de classes de pastas raízes, 
+digamos que você queira criar um cadastro que tenha valores padrão (como nesse exemplo), mas,
+existirão mais tipos de cadastro do que apenas aquele e ele será apenas o minímo que é necessário para se cadastrar (por exemplo), para que possamos especificar cada tipo de cadastro nós criariamos subpastas com o nome de cada tipo (cliente,fornecedor,etc) mas para que não haja conflito entre 
+as classes (devido ao nome) usamos o namespace, no caso dessa podemos ver que é o nome desta classe é cadastro mas ela está dentro do namespace cliente sendo assim cadastro próprio para cliente mesmo que herde os atributos padrão de cadastro.
+*/
 namespace Cliente;
 
-class Cadastro extends \Cadastro {
+class Cadastro extends /* o "\" é usado para indicar caminho, caso não tivesse ela ele ia estender a classe de si próprio. */\Cadastro {
+	
+
+	private $epica;
+
+	public function getEpica(){
+		return $this->epica;
+	}
+
+	public function setEpica($v){
+		$this->epica = $v;
+	}
+
 	public function registrarVenda(){
+		if ($this->getEpica() == "sim") {
+			echo "O vendedor ".$this->getNome()." é uma pica, ";
+		}
 		echo "Foi registrado uma venda pelo cliente :".$this->getNome();
 	}	 
 }

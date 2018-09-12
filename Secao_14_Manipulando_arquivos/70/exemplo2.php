191d93566174559b3234f4c2449adcb19eb6a903
viniciusbeckerbernardini <vbkmma@gmail.com>
2018-09-12 09:41:09 -0300

    Adicionei mais comentários

--- Secao_14_Manipulando_arquivos/70/exemplo2.php
+++ Secao_14_Manipulando_arquivos/70/exemplo2.php
@@ -1,14 +1,17 @@
 <?php 
-// 
+// 	Verificando se não existe o diretório images, caso não exista ele cria
 if (!is_dir("images")){
 	mkdir("images");
 }
-
+print_r(scandir("images"));
+// Transformando os itens que existem dentro de images em um array
+// acessando um a um usando o foreach
 foreach (scandir("images") as $item) {
-	if(!in_array($item, ['.','..']))
-
-	unlink("images".DIRECTORY_SEPARATOR.$item);
-
+	// Verifcando se não existe no array o item ou os outros 2 pontos
+	if(!in_array($item, ['.','..'])){
+		// Caso tudo ocorra certo ele excluí todas as fotos que estão no array
+		unlink("images".DIRECTORY_SEPARATOR.$item);
+	}
 }
 
 ?>
